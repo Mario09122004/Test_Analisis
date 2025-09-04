2eb0fc2cf3dad751d1c2189b844c3aa6d440b237
MarioAlbertoLiraZamora <mariorapido5560@hotmail.com>
2025-09-04 16:39:48 -0600

    Tab web

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,13 +1,10 @@
 "use client";
 
-import { UserProfile } from "@clerk/nextjs";
-
 export default function Users() {
 
   return (
     <div>
       Users
-      <UserProfile /> 
     </div>
   )
 }
